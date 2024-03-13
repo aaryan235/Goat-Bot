03094c618c7e1d1ab980e9c643dbe976ebaa657e
gupta4321 <156604313+gupta4321@users.noreply.github.com>
2024-03-13 12:09:46 +0530

    Update stalk.js

--- scripts/cmds/stalk.js
+++ scripts/cmds/stalk.js
@@ -1,88 +1,101 @@
-function convert(time){
-	var date = new Date(`${time}`);
-	var year = date.getFullYear();
-	var month = date.getMonth() + 1;
-	var day = date.getDate();
-	var hours = date.getHours();
-	var minutes = date.getMinutes();
-	var seconds = date.getSeconds();
-	var formattedDate = `${ day < 10 ? "0" + day : day}` + "/" +`${ month < 10 ? "0" + month : month}` + "/" + year + "||" + `${ hours < 10 ? "0" + hours : hours}` + ":" + `${ minutes < 10 ? "0" + minutes : minutes}` + ":" + `${ seconds < 10 ? "0" + seconds : seconds}`;
-	return formattedDate;
+const axios = require("axios");
+const fs = require("fs");
+const request = require("request");
+
+
+function convert(time) {
+  const date = new Date(time);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const formattedDate = `${day < 10 ? "0" + day : day}/${month < 10 ? "0" + month : month}/${year}||${hours < 10 ? "0" + hours : hours}:${minutes < 10 ? "0" + minutes : minutes}:${seconds < 10 ? "0" + seconds : seconds}`;
+  return formattedDate;
 }
 
+const headers = {
+  "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like) Version/12.0 eWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1",
+  "accept": "application/json, text/plain, /"
+};
+
 module.exports = {
-	config: {
-		name: "stalk",
-		author: "EUGENE",
-		version: "1.5",
-		countDown: 5,
-		role: 0,
-		guide: "[reply/uid/@mention]",
-		category: "info",
-		shortDescription: {
-			en: "Get info using uid/mention/reply to a message"
-		}
-	},
+  config: {
+    name: "stalk",
+    aliases: ["fbstalk"],
+    version: "1.0",
+    author: "Deku X kshitiz",
+    countDown: 5,
+    role: 0,
+    shortDescription: "Get info using uid/mention/reply to a message",
+    longDescription: {
+      en: "Get info using uid/mention/reply to a message",
+    },
+    category: "𝗜𝗡𝗙𝗢",
+    guide: {
+      en: "{pn}reply/uid/@mention",
+    },
+  },
 
-	onStart: async function({ api, event, args }) {
-		const request = require("request");
-		const axios = require("axios");
-		const fs = require("fs");
-		let path = __dirname + `/cache/info.png`;
+  onStart: async function ({ api, event, args }) {
+    let path = __dirname + `/cache/info.png`;
+    const token = "EAABwzLixnjYBOzbuqBHR3uitkbZAeCdSrxfdIJYvVx6MNekA9UO33lb2ztdb1Ohf4bprsMkiW1rmvl5zODiP12ilLZANZBafZCOdoPBHYcGjPyMP8IQZBke9VZBLzs8sla54Hf0ZCn7wZC9VTXfXhL7RUoG0b58pugqtYfcMWTrZBENQxYrZC72awGffLnf7IdyfElfIZATbtbgS4ZCB"; 
+    let id;
+    if (args.join().includes('@')) {
+      id = Object.keys(event.mentions)[0];
+    } else {
+      id = args[0] || event.senderID;
+    }
 
-		if (args.join().indexOf('@') !== -1) {
-			var id = Object.keys(event.mentions);
-		} else {
-			var id = args[0] || event.senderID;
-		}
+    if (event.type === "message_reply") {
+      id = event.messageReply.senderID;
+    }
 
-		if (event.type == "message_reply") {
-			var id = event.messageReply.senderID;
-		}
+    try {
+      const resp = await axios.get(`https://graph.facebook.com/${id}?fields=id,is_verified,cover,created_time,work,hometown,username,link,name,locale,location,about,website,birthday,gender,relationship_status,significant_other,quotes,first_name,subscribers.limit(0)&access_token=${token}`, { headers });
 
-		try {
-			const resp = await axios.get(`http://fi3.bot-hosting.net:20284/stalk?uid=${id}`);
-			var name = resp.data.name;
-			var link_profile = resp.data.link;
-			var uid = resp.data.id;
-			var first_name = resp.data.first_name;
-			var username = resp.data.username || "No data!";
-			var created_time = convert(resp.data.created_time);
-			var web = resp.data.website || "No data!";
-			var gender = resp.data.gender;
-			var relationship_status = resp.data.relationship_status || "No data!";
-			var love = resp.data.significant_other || "No data!";
-			var bday = resp.data.birthday || "No data!";
-			var follower = resp.data.subscribers.summary.total_count || "No data!";
-			var is_verified = resp.data.is_verified;
-			var quotes = resp.data.quotes || "No data!";
-			var about = resp.data.about || "No data!";
-			var locale = resp.data.locale || "No data!";
-			var hometown = !!resp.data.hometown ? resp.data.hometown.name : "No Hometown";
-			var cover = resp.data.source || "No Cover photo";
-			var avatar = `https://graph.facebook.com/${id}/picture?width=1500&height=1500&access_token=1174099472704185|0722a7d5b5a4ac06b11450f7114eb2e9`;
+      const name = resp.data.name;
+      const link_profile = resp.data.link;
+      const uid = resp.data.id;
+      const first_name = resp.data.first_name;
+      const username = resp.data.username || "No data!";
+      const created_time = convert(resp.data.created_time);
+      const web = resp.data.website || "No data!";
+      const gender = resp.data.gender;
+      const relationship_status = resp.data.relationship_status || "No data!";
+      const love = resp.data.significant_other || "No data!";
+      const bday = resp.data.birthday || "No data!";
+      const follower = resp.data.subscribers.summary.total_count || "No data!";
+      const is_verified = resp.data.is_verified;
+      const quotes = resp.data.quotes || "No data!";
+      const about = resp.data.about || "No data!";
+      const locale = resp.data.locale || "No data!";
+      const hometown = !!resp.data.hometown ? resp.data.hometown.name : "No Hometown";
+      const cover = resp.data.cover || "No Cover photo";
+      const avatar = `https://graph.facebook.com/${id}/picture?width=1500&height=1500&access_token=1174099472704185|0722a7d5b5a4ac06b11450f7114eb2e9`;
 
-			//callback
-let cb = function() {
-api.sendMessage({ body: `•——𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗧𝗜𝗢𝗡——•
-Name: ${name}
-First name: ${first_name}
-Creation Date: ${created_time}
-Profile link: ${link_profile}
-Gender: ${gender}
-Relationship Status: ${relationship_status}
-Birthday: ${bday}
-Follower(s): ${follower}
-Verified: ${is_verified}
-Hometown: ${hometown}
-Locale: ${locale}
-•——𝗘𝗡𝗗——•`, attachment: fs.createReadStream(path)
-				}, event.threadID, () => fs.unlinkSync(path), event.messageID);
-			};
 
-			request(encodeURI(avatar)).pipe(fs.createWriteStream(path)).on("close", cb);
-		} catch (err) {
-			api.sendMessage(`Error: ${err.message}`, event.threadID, event.messageID);
-		}
-	}
+      const cb = function () {
+        api.sendMessage({
+          body: `•——INFORMATION——•
+    Name: ${name}
+    First name: ${first_name}
+    Creation Date: ${created_time}
+    Profile link: ${link_profile}
+    Gender: ${gender}
+    Relationship Status: ${relationship_status}
+    Birthday: ${bday}
+    Follower(s): ${follower}
+    Hometown: ${hometown}
+    Locale: ${locale}
+    •——END——•`,
+          attachment: fs.createReadStream(path)
+        }, event.threadID, () => fs.unlinkSync(path), event.messageID);
+      };
+      request(encodeURI(avatar)).pipe(fs.createWriteStream(path)).on("close", cb);
+    } catch (err) {
+      api.sendMessage(`Error: ${err.message}`, event.threadID, event.messageID);
+    }
+  }
 };
