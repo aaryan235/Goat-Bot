093beafb85552b8bcacac9489d507713eb5e3d62
gupta4321 <156604313+gupta4321@users.noreply.github.com>
2024-03-16 15:39:04 +0530

    Update imgur.js

--- scripts/cmds/imgur.js
+++ scripts/cmds/imgur.js
@@ -18,13 +18,13 @@ module.exports = {
 
   onStart: async function ({ api, event }) {
     const dip = event.messageReply?.attachments[0]?.url;
-    if (!dip) {
+    if (!link2) {
       return api.sendMessage('Please reply to an image or video.', event.threadID, event.messageID);
     }
     try {
-      const res = await axios.get(`https://imgur-api-yd3t.onrender.com/dip?url=${encodeURIComponent(dip)}`);
-      const dipto = res.data.data;
-         api.sendMessage(dipto, event.threadID, event.messageID);
+      const res = await axios.get(`https://eurix-api.replit.app/imgur?link=${encodeURIComponent(link2)}`);
+      const link2 = res.data.data;
+         api.sendMessage(link2, event.threadID, event.messageID);
     } catch (error) {
       console.error(error);
       return api.sendMessage('Failed to convert image or video into link.', event.threadID, event.messageID);
