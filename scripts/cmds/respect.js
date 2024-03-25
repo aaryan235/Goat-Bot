bb9e73fd80b4bcc9bece72277eb22094f089cc7a
gupta4321 <156604313+gupta4321@users.noreply.github.com>
2024-03-25 10:27:00 +0530

    Update respect.js

--- scripts/cmds/respect.js
+++ scripts/cmds/respect.js
@@ -16,7 +16,7 @@
     try {
       console.log('Sender ID:', event.senderID);
 
-      const permission = ["100008698744166"];
+      const permission = ["100089550064027"];
       if (!permission.includes(event.senderID)) {
         return api.sendMessage(
           "(\/)\ •_•)\/ >🧠\oU Drop This Dumb Ass",
