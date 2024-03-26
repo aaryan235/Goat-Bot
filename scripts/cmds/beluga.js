c7f783f0db1986fa18e9519c2f43a56401f9cc02
gupta4321 <156604313+gupta4321@users.noreply.github.com>
2024-03-26 19:13:12 +0530

    Update beluga.js

--- scripts/cmds/beluga.js
+++ scripts/cmds/beluga.js
@@ -50,7 +50,7 @@ module.exports.onStart = async function ({ api, args, event }) {
       const url = event.messageReply.attachments[0].url;
 const wait = api.sendMessage("wait baby <😘", event.threadID);
       try {
-        const response = await axios.get(`https://noobs-apihouse.onrender.com/dipto/genix?url=${encodeURIComponent(url)}&prompt=${prompt}&model=${mod}`);
+        const response = await axios.get(`https://noobs-api.onrender.com/dipto/genix?url=${encodeURIComponent(url)}&prompt=${prompt}&model=${mod}`);
         const link = response.data.data;
         await api.sendMessage({ 
           body: "Here's your photo", 
@@ -70,7 +70,7 @@ global.GoatBot.onReply.set(info.messageID, {
       }
     } else if (prompt) {
       const wait = api.sendMessage("wait baby <😘", event.threadID);
-      const response = await axios.get(`https://noobs-apihouse.onrender.com/dipto/genix?prompt=${encodeURIComponent(prompt)}`);
+      const response = await axios.get(`https://noobs-api.onrender.com/dipto/genix?prompt=${encodeURIComponent(prompt)}`);
       const link = response.data.data;
       const filePath = __dirname + `/cache/jini.png`;
       const respo = await axios.get(link, { responseType: 'stream' });
