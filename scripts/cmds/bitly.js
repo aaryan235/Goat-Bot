66d2d165f35b9072c13f6b7498a4b808f1f86615
gupta4321 <156604313+gupta4321@users.noreply.github.com>
2024-04-15 17:05:49 +0530

    Update bitly.js

--- scripts/cmds/bitly.js
+++ scripts/cmds/bitly.js
@@ -1,22 +1,23 @@
 const axios = require('axios');
 
 const Prefixes = [
-   'ai',
-  'alexa'
+  'ai',
+  'AI',
+  'Ai'
 ];
 
 module.exports = {
   config: {
     name: 'ai',
-    version: '2.5',
-    author: 'JV Barcenas', // do not change
+    version: '2.6.2',
+    author: 'JV Barcenas | Shikaki', // do not change
     role: 0,
     category: 'ai',
     shortDescription: {
-      en: 'Asks an AI for an answer.',
+      en: 'Asks gemini AI for an answer.',
     },
     longDescription: {
-      en: 'Asks an AI for an answer based on the user prompt.',
+      en: 'Asks gemini AI for an answer based on the user prompt.',
     },
     guide: {
       en: '{pn} [prompt]',
@@ -35,31 +36,28 @@ module.exports = {
 
       if (prompt === '') {
         await message.reply(
-          "My lord what's your question."
+          "Kindly provide the question at your convenience and I shall strive to deliver an effective response. Your satisfaction is my top priority."
         );
-        return;   
+        return;
       }
 
+      api.setMessageReaction("⌛", event.messageID, () => { }, true);
 
-      await message.reply("Answering your question my Lord. Please wait a moment...");
+      let updatedPrompt = `Follow as written: Mostly answer in 1 word or 1 sentene. For any affirmation to your answers only yes or no. Answer in 1-2 sentences for generic questions and longer for complex questions. Mostly stick to 1 sentences unless asked long answers. Now: ${prompt}`;
 
-      const response = await axios.get(`https://api.easy-api.online/v1/globalgpt?q=${encodeURIComponent(prompt)}`);
+      const response = await axios.get(`https://pi.aliestercrowley.com/api?prompt=${encodeURIComponent(updatedPrompt)}&uid=${event.senderID}`);
 
       if (response.status !== 200 || !response.data) {
         throw new Error('Invalid or missing response from API');
       }
 
-      const messageText = response.data.content.trim();
+      const messageText = response.data.response;
 
       await message.reply(messageText);
 
-      console.log('Sent answer as a reply to user');
+      api.setMessageReaction("✅", event.messageID, () => { }, true);
     } catch (error) {
-      console.error(`Failed to get answer: ${error.message}`);
-      api.sendMessage(
-        `${error.message}.\n\nYou can try typing your question again or resending it, as there might be a bug from the server that's causing the problem. It might resolve the issue.`,
-        event.threadID
-      );
+      message.reply(`Failed to get answer: ${error.message}`);
     }
   },
 };
