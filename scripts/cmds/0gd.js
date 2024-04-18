616bc8684f636bed9d434a389448ab4cd6a3652e
gupta4321 <156604313+gupta4321@users.noreply.github.com>
2024-04-18 20:47:26 +0530

    Update 0gd.js

--- scripts/cmds/0gd.js
+++ scripts/cmds/0gd.js
@@ -1,45 +1,58 @@
-const axios = require('axios');
+const { writeFileSync, existsSync, mkdirSync } = require("fs");
+const { join } = require("path");
+const axios = require("axios");
+const tinyurl = require('tinyurl');
+const fs = require('fs');
 
 module.exports = {
   config: {
-    name: "drawever",
+    name: "colorize",
+    aliases: [],
     version: "1.0",
-    author: "Rehat86, Chat GPT",
-    category: "ai",
+    author: "Kshitiz",
+    countDown: 20,
     role: 0,
-    longDescription: {
-      en:"Convert photo to anime art image."
-    },
-    guide: {en:"{pn} <reply_image>"}
-  },
-  onStart: async function ({ message, event }) {
-    if (
-      event.type === "message_reply" &&
-      event.messageReply.attachments &&
-      event.messageReply.attachments.length > 0 &&
-      ["photo", "sticker"].includes(event.messageReply.attachments[0].type)
-    ) {
-      const imageUrl = event.messageReply.attachments[0].url;
-      const encodedImageUrl = encodeURIComponent(imageUrl);
-message.reply("Please wait...⏳");
-      try {
-        const response = await axios.get(`https://api-drawever-d08d4f91d6f0.herokuapp.com/api/drawever?imgurl=${encodedImageUrl}`);
-        const mixedImageUrl = response.data.urls.mixed;
-        
-        if (mixedImageUrl) {
-          const imageStream = await global.utils.getStreamFromURL(mixedImageUrl);
-          await message.reply({
-            attachment: [imageStream]
-          });
-        } else {
-          message.reply("An error occurred.");
-        }
-      } catch (error) {
-        console.error(error);
-         message.reply("An error occurred.");
-      }
-    } else {
-     message.reply("Please reply with an image.");
+    shortDescription: "Colorize an image",
+    longDescription: "Colorize a b&w image",
+    category: "image",
+    guide: {
+      en: "{p}colorize (reply to image)"
     }
   },
+
+  onStart: async function ({ message, event, api }) {
+    api.setMessageReaction("🕐", event.messageID, (err) => {}, true);
+    const { type, messageReply } = event;
+    const { attachments, threadID, messageID } = messageReply || {};
+
+    if (type === "message_reply" && attachments) {
+      const [attachment] = attachments;
+      const { url, type: attachmentType } = attachment || {};
+
+      if (!attachment || attachmentType !== "photo") {
+        return message.reply("Reply to an image.");
+      }
+
+      try {
+        const tinyUrl = await tinyurl.shorten(url);
+        const apiUrl = `https://colo-rize.vercel.app/kshitiz?url=${encodeURIComponent(tinyUrl)}`;
+        const imageResponse = await axios.get(apiUrl, { responseType: "arraybuffer" });
+
+        const cacheDir = join(__dirname, "cache");
+        if (!existsSync(cacheDir)) {
+          mkdirSync(cacheDir, { recursive: true });
+        }
+
+        const imagePath = join(cacheDir, "colorized_image.png");
+        writeFileSync(imagePath, imageResponse.data);
+
+        message.reply({ attachment: fs.createReadStream(imagePath) }, threadID);
+      } catch (error) {
+        console.error(error);
+        message.reply("❌");
+      }
+    } else {
+      message.reply("Please reply to an image.");
+    }
+  }
 };
